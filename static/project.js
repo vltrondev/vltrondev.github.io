d1ec088adfb9288592bea30a6fd9c80dc054126e
vltrondev <samuelaquinoi@hotmail.com>
2024-05-03 11:39:54 -0700

    Agregando el proyecto

---
+++ static/project.js
@@ -0,0 +1,55 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/* Project specific Javascript goes here. */
+
+/*
+Formatting hack to get around crispy-forms unfortunate hardcoding
+in helpers.FormHelper:
+
+    if template_pack == 'bootstrap4':
+        grid_colum_matcher = re.compile('\w*col-(xs|sm|md|lg|xl)-\d+\w*')
+        using_grid_layout = (grid_colum_matcher.match(self.label_class) or
+                             grid_colum_matcher.match(self.field_class))
+        if using_grid_layout:
+            items['using_grid_layout'] = True
+
+Issues with the above approach:
+
+1. Fragile: Assumes Bootstrap 4's API doesn't change (it does)
+2. Unforgiving: Doesn't allow for any variation in template design
+3. Really Unforgiving: No way to override this behavior
+4. Undocumented: No mention in the documentation, or it's too hard for me to find
+*/
+
+
+}
+/*
+     FILE ARCHIVED ON 16:39:23 Apr 08, 2019 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 02:58:13 May 03, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.698
+  exclusion.robots: 0.084
+  exclusion.robots.policy: 0.073
+  cdx.remote: 0.065
+  esindex: 0.01
+  LoadShardBlock: 102.248 (3)
+  PetaboxLoader3.datanode: 182.856 (5)
+  PetaboxLoader3.resolve: 315.351 (3)
+  load_resource: 2831.698 (2)
+*/
